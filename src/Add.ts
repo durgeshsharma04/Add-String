7e2b914adb77ea08e7a8a565bdc444f4bbbf166a
Durgesh Sharma <durgesh.sharma@vlinkinfo.com>
2024-06-19 18:53:49 +0530

    add regex for replace string

--- src/Add.ts
+++ src/Add.ts
@@ -2,13 +2,16 @@ export function add(a: String) {
     if(a == "" || a.length  == 1){
         return Number(a);
     }
-    if(a.includes('\n')){
-        a = a.replace("\n",",");
-    }
-    let num = a.split(",")
+    a = a.replace(/\n|;|\//g,",");
+    let num = a.split(",");
     let total : number = 0
     num.forEach((val)=>{
-        total = total + Number(val);
+        if(val == "" || Number.isNaN(Number(val))){
+            total = total
+        }else{
+            total = total + Number(val);
+        }
+        
     })
     return total
   }
