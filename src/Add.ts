5bacc5787d2d0cd715502ac8d2ca82300e566c92
Durgesh Sharma <durgesh.sharma@vlinkinfo.com>
2024-06-19 17:00:29 +0530

    enhance function

--- src/Add.ts
+++ src/Add.ts
@@ -2,6 +2,9 @@ export function add(a: String) {
     if(a == "" || a.length  == 1){
         return Number(a);
     }
+    if(a.includes('\n')){
+        a = a.replace("\n",",");
+    }
     let num = a.split(",")
     let total : number = 0
     num.forEach((val)=>{
