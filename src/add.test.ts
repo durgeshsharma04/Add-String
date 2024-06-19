c814b33e0d50cb73b6ec42efe6fb24ffbd32e7d9
Durgesh Sharma <durgesh.sharma@vlinkinfo.com>
2024-06-19 15:31:09 +0530

    add new test

--- src/add.test.ts
+++ src/add.test.ts
@@ -4,4 +4,7 @@ describe("Add functions", () => {
   it("test with single integer", () => {
     expect(add("1")).toEqual(1);
   });
+  it("test with empty string", () => {
+    expect(add("")).toEqual(0);
+  });
 });
