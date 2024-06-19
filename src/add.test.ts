509e66ec1a13da8c780d2273e12c61f39d976173
Durgesh Sharma <durgesh.sharma@vlinkinfo.com>
2024-06-19 15:33:34 +0530

    add test case of multiple number

--- src/add.test.ts
+++ src/add.test.ts
@@ -7,4 +7,7 @@ describe("Add functions", () => {
   it("test with empty string", () => {
     expect(add("")).toEqual(0);
   });
+  it("test with comma seprated number", () => {
+    expect(add("1,2,3,4")).toEqual(10);
+  });
 });
